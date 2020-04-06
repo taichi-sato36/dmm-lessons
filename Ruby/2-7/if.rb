be56cbfddd5457803f2d00d9f551ae71ca958cc7
Taichi Satou <taichi@mpb.local>
2020-04-06 22:21:01 +0900

    [finish]2-7

---
+++ Ruby/2-7/if.rb
@@ -0,0 +1,27 @@
+apple = 110
+
+if apple >= 100
+	puts "リンゴの値段は100円以上です"
+end
+
+if apple <= 100
+	puts "リンゴの値段は100円以下です"
+end
+
+tall = 180
+
+if tall >= 170 && tall <= 190
+	puts "身長は170以上、190以下ですね"
+end
+
+ringo = "yamagata"
+
+if ringo == "Nagano"
+	puts "リンゴの生産地は長野です"
+elsif ringo == "yamagata"
+	puts "リンゴの生産地は山形です"
+else
+	puts "リンゴの生産地は青森です"
+end
+
+
